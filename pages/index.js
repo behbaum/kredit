d7f397b6adf97c502016718dfc79dedb222d92b3
behbaum <73855290+behbaum@users.noreply.github.com>
2020-12-17 18:26:41 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://kreditit.de">KRΞDEBit</a>
+          Welcome to <a href="https://kreditit.de">KREDΞBit</a>
         </h1>
 
         <p className={styles.description}>
