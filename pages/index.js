4ac14c8947ce63a574bd5421ab5b187bdac2d6eb
behbaum <73855290+behbaum@users.noreply.github.com>
2020-12-17 17:37:18 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,40 +11,40 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://kreditit.de">KRΞDITit</a>
+          Welcome to <a href="https://kreditit.de">KRΞDEBit</a>
         </h1>
 
         <p className={styles.description}>
-          Where banking meets{' '}
-          <code className={styles.code}>technology</code>
+          Business & Finance Tools for{' '}
+          <code className={styles.code}>Global Citizens</code>
         </p>
 
         <div className={styles.grid}>
    
-          <a href="https://banking.kreditit.de" className={styles.card}>
-            <h3>Banking Solutions &rarr;</h3>
-            <p>Learn about our banking & investment solutions.</p>
+          <a href="https://kredibit.de/launchpad/" className={styles.card}>
+            <h3>LaunchPad &rarr;</h3>
+            <p>Expand your branches in our 30+ global locations.</p>
           </a>
           <a
-            href="https://smb.kreditit.de"
+            href="https://kredibit.de/"
             className={styles.card}
           >
             <h3>SMB Credits &rarr;</h3>
             <p>
-              Apply for our special loans for SMBs & Startups.
+              Apply for our special credits for SMBs & Startups.
             </p>
           </a>
           <a
-            href="https://partners.kreditit.de"
+            href="https://kredibit.de/about/"
             className={styles.card}
           >
             <h3>Partners & Investors &rarr;</h3>
-            <p>Discover our investment & partnership opportunities.</p>
+            <p>Discover investment & partnership opportunities.</p>
           </a>
        
-        <a href="https://servicedesk.kreditit.de" className={styles.card}>
-            <h3>Service Desk &rarr;</h3>
-            <p>Connect to our cloud of virtual office services.</p>
+        <a href="https://accounts.zoho.com/signin?servicename=VirtualOffice" className={styles.card}>
+            <h3>Virtual Office &rarr;</h3>
+            <p>Login to our virtual office here.</p>
           </a>
           
         </div>
